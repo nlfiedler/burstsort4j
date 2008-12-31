1f289f229cdeead7545c18aeb7b4412738a074a3
Nathan Fiedler <nathanfiedler@fastmail.fm>
2008-12-31 00:27:35 +0000

    #12: take file arguments for benchmarking

--- src/org/burstsort4j/Benchmark.java
+++ src/org/burstsort4j/Benchmark.java
@@ -19,6 +19,10 @@
 
 package org.burstsort4j;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -42,10 +46,42 @@ public class Benchmark {
      * @param  args  command-line arguments.
      */
     public static void main(String[] args) {
-        DataGenerator[] generators = new DataGenerator[] {
-            new RandomGenerator(),
-            new PsuedoWordGenerator(),
-        };
+        DataGenerator[] generators = null;
+        DataSize[] sizes = null;
+        if (args.length > 0) {
+            if (args.length != 2) {
+                System.out.println("Please provide two arguments: --1|--2|--3 <filename>");
+                System.exit(1);
+            }
+            // Must provide size argument followed by file name.
+            String size = args[0];
+            if (size.equals("--3")) {
+                sizes = DataSize.values();
+            } else if (size.equals("--2")) {
+                sizes = new DataSize[] { DataSize.SMALL, DataSize.MEDIUM };
+            } else if (size.equals("--1")) {
+                sizes = new DataSize[] { DataSize.SMALL };
+            } else {
+                System.err.println("First argument must be size (--1, --2, or --3)");
+                System.exit(1);
+            }
+            File file = new File(args[1]);
+            if (!file.exists()) {
+                System.err.format("File '%s' not found!\n", args[1]);
+                System.exit(1);
+            }
+            generators = new DataGenerator[] {
+                new FileGenerator(file)
+            };
+        } else {
+            // With no arguments, run the random data generators and all
+            // of the data sizes.
+            generators = new DataGenerator[] {
+                new RandomGenerator(),
+                new PsuedoWordGenerator()
+            };
+            sizes = DataSize.values();
+        }
         SortRunner[] runners = new SortRunner[] {
             new MergesortRunner(),
             new QuicksortRunner(),
@@ -53,6 +89,25 @@ public class Benchmark {
             new Multikey2Runner(),
             new BurstsortRunner(),
         };
+        try {
+            runsorts(generators, runners, sizes);
+        } catch (GeneratorException ge) {
+            ge.printStackTrace();
+        }
+    }
+
+    /**
+     * Runs a set of sort routines over test data, as provided by the
+     * given data generators. Performs a warmup run first to get all
+     * of the classes compiled by the JVM, to avoid skewing the resuls.
+     *
+     * @param  generators  set of data generators to use.
+     * @param  runners     set of sorters to compare.
+     * @param  sizes       data sizes to be run.
+     * @throws  GeneratorException  thrown if one of the generators fails.
+     */
+    private static void runsorts(DataGenerator[] generators,
+            SortRunner[] runners, DataSize[] sizes) throws GeneratorException {
         // Warm up the JVM so that the classes get compiled and the
         // CPU comes up to full speed.
         System.out.println("Warming up the system, please wait...");
@@ -69,7 +124,7 @@ public class Benchmark {
         // calculate an average run time.
         for (DataGenerator generator : generators) {
             System.out.format("%s...\n", generator.getDisplayName());
-            for (DataSize size : DataSize.values()) {
+            for (DataSize size : sizes) {
                 System.out.format("\t%s...\n", size.toString());
                 List<String> data = generator.generate(size);
                 for (SortRunner runner : runners) {
@@ -99,6 +154,32 @@ public class Benchmark {
     }
 
     /**
+     * Checked exception for the data generators.
+     */
+    private static class GeneratorException extends Exception {
+        /** silence compiler warnings */
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * GeneratorException with a message.
+         *
+         * @param  msg  explanatory message.
+         */
+        public GeneratorException(String msg) {
+            super(msg);
+        }
+
+        /**
+         * GeneratorException with a cause.
+         *
+         * @param  cause  cause of the exception.
+         */
+        public GeneratorException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+    /**
      * Creates a set of data to be sorted.
      */
     private static interface DataGenerator {
@@ -108,8 +189,9 @@ public class Benchmark {
          *
          * @param  size  size of the data to be generated.
          * @return  list of strings.
+         * @throws  GeneratorException  thrown if generation fails.
          */
-        List<String> generate(DataSize size);
+        List<String> generate(DataSize size) throws GeneratorException;
 
         /**
          * Returns the display name for this generator.
@@ -117,6 +199,65 @@ public class Benchmark {
          * @return  display name.
          */
         String getDisplayName();
+    }
+
+    /**
+     * A "generator" that reads data from a named file, returning
+     * a particular number of lines based on the requested size.
+     * The file must have sufficient data or an error occurs.
+     */
+    private static class FileGenerator implements DataGenerator {
+        /** File from whence data is to be read. */
+        private File file;
+
+        /**
+         * Constructor for FileGenerator, which reads from the given file.
+         *
+         * @param  file  that which contains test data.
+         */
+        public FileGenerator(File file) {
+            this.file = file;
+        }
+
+        @Override
+        public List<String> generate(DataSize size) throws GeneratorException {
+            int count = 0;
+            switch (size) {
+                case SMALL:
+                    count = 100000;
+                    break;
+                case MEDIUM:
+                    count = 1000000;
+                    break;
+                case LARGE:
+                    count = 3000000;
+                    break;
+            }
+            List<String> data = new ArrayList<String>(count);
+            try {
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                String line = br.readLine();
+                while (line != null && count > 0) {
+                    data.add(line);
+                    count--;
+                    line = br.readLine();
+                }
+            } catch (IOException ioe) {
+                throw new GeneratorException(ioe);
+            }
+            if (count > 0) {
+                throw new GeneratorException(String.format(
+                        "File '%s' has too few lines (%d more needed)",
+                        file.getName(), count));
+            }
+            return data;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return file.getName();
+        }
     }
 
     /**
@@ -131,7 +272,7 @@ public class Benchmark {
         private static final int ALPHABET = 26;
 
         @Override
-        public List<String> generate(DataSize size) {
+        public List<String> generate(DataSize size) throws GeneratorException {
             int count = 0;
             switch (size) {
                 case SMALL:
@@ -176,7 +317,7 @@ public class Benchmark {
         private static final int ALPHABET = 62;
 
         @Override
-        public List<String> generate(DataSize size) {
+        public List<String> generate(DataSize size) throws GeneratorException {
             int count = 0;
             switch (size) {
                 case SMALL:
