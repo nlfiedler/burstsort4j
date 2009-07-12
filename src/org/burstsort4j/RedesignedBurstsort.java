8bc14778e28c53a03cddda27ccb321d2761ee303
Nathan Fiedler <nathanfiedler@fastmail.fm>
2009-07-12 01:07:55 +0000

    Adding single-threaded implementation of bucket-redesign burstsort.

---
+++ src/org/burstsort4j/RedesignedBurstsort.java
@@ -0,0 +1,460 @@
+/*
+ * Copyright (C) 2009  Nathan Fiedler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
+package org.burstsort4j;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Array-based implementation of the redesigned Burstsort, referred to as
+ * P+BR-Burstsort. There are both single-threaded and multi-threaded
+ * implementations, designed to take advantage of multiple CPU cores,
+ * if available.
+ *
+ * <p>The redesigned burstsort is more memory efficient than the original
+ * burstsort, but may run slower as a result of the additional memory
+ * allocations and array copies that are performed.</p>
+ *
+ * @author Nathan Fiedler
+ */
+public class RedesignedBurstsort {
+    /** Null terminator character. */
+    private static final char NULLTERM = '\0';
+    /** Maximum number of elements in any given bucket (except null bucket). */
+    private static final short THRESHOLD = 8192;
+    /** Size of the alphabet that is supported. */
+    private static final short ALPHABET = 256;
+    /** Initial size for new sub-buckets. Combined with the growth factor
+     * it is crucial that the sub-bucket threshold is contained in the
+     * resulting sequence (e.g. 2, 4, 8, 16, 32, 64, 128, 256). */
+    private static final short SUBBUCKET_START_SIZE = 16;
+    /** The sub-bucket growth factor. The growth factor must strike a
+     * balance between spending time allocating and copying arrays and
+     * wasting space. */
+    private static final short SUBBUCKET_GROWTH_FACTOR = 2;
+    /** Size of the sub-buckets referenced within the bucket index. */
+    private static final short SUBBUCKET_THRESHOLD = 256;
+
+    /**
+     * Creates a new instance of RedesignedBurstsort.
+     */
+    private RedesignedBurstsort() {
+    }
+
+    /**
+     * Retrieve the character in string s at offset d. If d is greater
+     * than or equal to the length of the string, return zero. This
+     * simulates fixed-length strings that are zero-padded.
+     *
+     * @param  s  string.
+     * @param  d  offset.
+     * @return  character in s at d, or zero.
+     */
+    private static final char charAt(CharSequence s, int d) {
+        return d < s.length() ? s.charAt(d) : NULLTERM;
+    }
+
+    /**
+     * Inserts a set of strings into the burst trie structure, in
+     * preparation for in-order traversal (hence sorting).
+     *
+     * @param  root     root of the structure.
+     * @param  strings  strings to be inserted.
+     */
+    private static void insert(Node root, CharSequence[] strings) {
+        for (int i = 0; i < strings.length; i++) {
+            // Start at root each time
+            Node curr = root;
+            // Locate trie node in which to insert string
+            int p = 0;
+            char c = charAt(strings[i], p);
+            while (curr.size(c) < 0) {
+                curr = (Node) curr.get(c);
+                p++;
+                c = charAt(strings[i], p);
+            }
+            curr.add(c, strings[i]);
+            // is bucket size above the THRESHOLD?
+            while (curr.size(c) >= THRESHOLD && c != NULLTERM) {
+                // advance depth of character
+                p++;
+                // allocate memory for new trie node
+                Node newt = new Node();
+                // burst...
+                char cc = NULLTERM;
+                Object[] bind = (Object[]) curr.get(c);
+                for (int j = 0; j < bind.length; /* see below */) {
+                    CharSequence[] sub = (CharSequence[]) bind[j];
+                    int limit = sub.length;
+                    j++;
+                    if (j == bind.length) {
+                        // Last sub-bucket may not be fully utilized.
+                        int last = curr.size(c) % SUBBUCKET_THRESHOLD;
+                        if (last > 0) {
+                            limit = last;
+                        }
+                    }
+                    for (int k = 0; k < limit; k++) {
+                        // access the next depth character
+                        cc = charAt(sub[k], p);
+                        newt.add(cc, sub[k]);
+                    }
+                }
+                // old pointer points to the new trie node
+                curr.set(c, newt);
+                // used to burst recursive, so point curr to new
+                curr = newt;
+                // point to character used in previous string
+                c = cc;
+            }
+        }
+    }
+
+    /**
+     * Sorts the set of strings using the original (P-)burstsort algorithm.
+     *
+     * @param  strings  array of strings to be sorted.
+     */
+    public static void sort(CharSequence[] strings) {
+        sort(strings, null);
+    }
+
+    /**
+     * Sorts the given set of strings using the original (P-)burstsort
+     * algorithm. If the given output stream is non-null, then metrics
+     * regarding the burstsort trie structure will be printed there.
+     *
+     * @param  strings  array of strings to be sorted.
+     * @param  out      if non-null, metrics are printed here.
+     */
+    public static void sort(CharSequence[] strings, PrintStream out) {
+        if (strings != null && strings.length > 1) {
+            Node root = new Node();
+            insert(root, strings);
+            if (out != null) {
+                writeMetrics(root, out);
+            }
+            traverse(root, strings, 0, 0);
+        }
+    }
+
+    /**
+     * Uses all available processors to sort the trie buckets in parallel,
+     * thus sorting the overal set of strings in less time. Uses a simple
+     * ThreadPoolExecutor with a maximum pool size equal to the number of
+     * available processors (usually equivalent to the number of CPU cores).
+     *
+     * @param  strings  array of strings to be sorted.
+     * @throws  InterruptedException  if waiting thread was interrupted.
+     */
+    public static void sortThreadPool(CharSequence[] strings) throws InterruptedException {
+        if (strings != null && strings.length > 1) {
+            Node root = new Node();
+            insert(root, strings);
+            List<Callable<Object>> jobs = new ArrayList<Callable<Object>>();
+            traverseParallel(root, strings, 0, 0, jobs);
+            ExecutorService executor = Executors.newFixedThreadPool(
+                    Runtime.getRuntime().availableProcessors());
+            // Using ExecutorService.invokeAll() usually adds more time.
+            for (Callable<Object> job : jobs) {
+                executor.submit(job);
+            }
+            executor.shutdown();
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        }
+    }
+
+    /**
+     * Traverse the trie structure, ordering the strings in the array to
+     * conform to their lexicographically sorted order as determined by
+     * the trie structure.
+     *
+     * @param  node     node within trie structure.
+     * @param  strings  the strings to be ordered.
+     * @param  pos      position within array.
+     * @param  deep     character offset within strings.
+     * @return  new pos value.
+     */
+    private static int traverse(Node node, CharSequence[] strings, int pos, int deep) {
+        for (char c = 0; c < ALPHABET; c++) {
+            int count = node.size(c);
+            if (count < 0) {
+                pos = traverse((Node) node.get(c), strings, pos, deep + 1);
+            } else if (count > 0) {
+                int off = pos;
+                // Copy the string references from the bucket to the
+                // destination. This is done for all node entries, even
+                // the null bucket.
+                Object[] bind = (Object[]) node.get(c);
+                for (int j = 0; j < bind.length; /* see below */) {
+                    CharSequence[] sub = (CharSequence[]) bind[j];
+                    int limit = sub.length;
+                    j++;
+                    if (j == bind.length) {
+                        // Last sub-bucket may not be fully utilized.
+                        int last = count % SUBBUCKET_THRESHOLD;
+                        if (last > 0) {
+                            limit = last;
+                        }
+                    }
+                    System.arraycopy(sub, 0, strings, off, limit);
+                    off += limit;
+                }
+                if (c > 0) {
+                    // Sort the strings that were just copied to the
+                    // destination now that they are all in one array.
+                    if (count > 1) {
+                        int high = pos + count;
+                        if (count < 20) {
+                            Insertionsort.sort(strings, pos, high, deep + 1);
+                        } else {
+                            MultikeyQuicksort.sort(strings, pos, high, deep + 1);
+                        }
+                    }
+                }
+                pos += count;
+            }
+        }
+        return pos;
+    }
+
+    /**
+     * Traverse the trie structure, creating jobs for each of the buckets.
+     *
+     * @param  node     node within trie structure.
+     * @param  strings  the strings to be ordered.
+     * @param  pos      position within array.
+     * @param  deep     character offset within strings.
+     * @param  jobs     job list to which new jobs are added.
+     * @return  new pos value.
+     */
+    private static int traverseParallel(Node node, CharSequence[] strings,
+            int pos, int deep, List<Callable<Object>> jobs) {
+        for (char c = 0; c < ALPHABET; c++) {
+            int count = node.size(c);
+            if (count < 0) {
+                pos = traverseParallel((Node) node.get(c), strings, pos,
+                        deep + 1, jobs);
+            } else if (count > 0) {
+                int off = pos;
+                if (c == 0) {
+// XXX
+//                    // Visit all of the null buckets, which are daisy-chained
+//                    // together with the last reference in each bucket pointing
+//                    // to the next bucket in the chain.
+//                    int no_of_buckets = (count / THRESHOLDMINUSONE) + 1;
+//                    Object[] nullbucket = (Object[]) node.get(c);
+//                    for (int k = 1; k <= no_of_buckets; k++) {
+//                        int no_elements_in_bucket;
+//                        if (k == no_of_buckets) {
+//                            no_elements_in_bucket = count % THRESHOLDMINUSONE;
+//                        } else {
+//                            no_elements_in_bucket = THRESHOLDMINUSONE;
+//                        }
+//                        jobs.add(new CopyJob(nullbucket, no_elements_in_bucket, strings, off));
+//                        off += no_elements_in_bucket;
+//                        nullbucket = (Object[]) nullbucket[no_elements_in_bucket];
+//                    }
+                } else {
+                    // A regular bucket with string tails that need to
+                    // be sorted and copied to the final destination.
+//                    CharSequence[] bucket = (CharSequence[]) node.get(c);
+//                    jobs.add(new SortJob(bucket, count, strings, off, deep + 1));
+                }
+                pos += count;
+            }
+        }
+        return pos;
+    }
+
+    /**
+     * Collect metrics regarding the burstsort trie structure and write
+     * them to the given output stream.
+     *
+     * @param  node  root node of the trie structure.
+     * @param  out   output stream to write to.
+     */
+    private static void writeMetrics(Node node, PrintStream out) {
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(node);
+        int nodes = 0;
+        int buckets = 0;
+        int consumedStrings = 0;
+        int nonEmptyBuckets = 0;
+        int smallest = Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE;
+        long sum = 0;
+        while (!stack.isEmpty()) {
+            node = stack.pop();
+            nodes++;
+            for (char c = 0; c < ALPHABET; c++) {
+                int count = node.size(c);
+                if (count < 0) {
+                    stack.push((Node) node.get(c));
+                } else {
+                    if (c == 0) {
+                        consumedStrings += count;
+                    }
+                    buckets++;
+                    // Only consider non-empty buckets, as there will
+                    // always be empty buckets.
+                    if (count > 0) {
+                        sum += count;
+                        if (count < smallest) {
+                            smallest = count;
+                        }
+                        nonEmptyBuckets++;
+                    }
+                    if (count > largest) {
+                        largest = count;
+                    }
+                }
+            }
+        }
+        out.format("Trie nodes: %d\n", nodes);
+        out.format("Total buckets: %d\n", buckets);
+        out.format("Consumed strings: %d\n", consumedStrings);
+        out.format("Smallest bucket: %d\n", smallest);
+        out.format("Largest bucket: %d\n", largest);
+        out.format("Average bucket: %d\n", sum / nonEmptyBuckets);
+    }
+
+    /**
+     * A node in the burst trie structure based on the redesigned Burstsort
+     * algorithm, consisting of a null tail pointer bucket and zero or more
+     * buckets for the other entries. Entries may point either to a bucket
+     * or another trie node. The buckets consist of a bucket index (BIND)
+     * which points to sub-buckets that grown on demand.
+     *
+     * @author  Nathan Fiedler
+     */
+    private static class Node {
+        /** Reference to the last sub-bucket in the index, if any. */
+        private final Object[] lastBucket = new Object[ALPHABET];
+        /** Next free slot in the last bucket, if there is a bucket. */
+        private final int[] lastIndex = new int[ALPHABET];
+        /** count of items in bucket, or -1 if reference to trie node */
+        private final int[] counts = new int[ALPHABET];
+        /** Pointers to bucket index or a trie node. */
+        private final Object[] ptrs = new Object[ALPHABET];
+
+        /**
+         * Add the given string into the appropriate bucket, given the
+         * character index into the trie. Presumably the character is
+         * from the string, but not necessarily so. The character may
+         * be the null character, in which case the string is added to
+         * the null bucket. Buckets are expanded as needed to accomodate
+         * the new string.
+         *
+         * @param  c  character used to index trie entry.
+         * @param  s  the string to be inserted.
+         */
+        public void add(char c, CharSequence s) {
+            // are buckets already created?
+            if (counts[c] < 1) {
+                // no, create the new bucket
+                CharSequence[] sub = new CharSequence[SUBBUCKET_START_SIZE];
+                sub[0] = s;
+                lastBucket[c] = sub;
+                lastIndex[c] = 1;
+                // Make bucket index (BIND) sized-to-fit.
+                Object[] bind = new Object[1];
+                bind[0] = sub;
+                ptrs[c] = bind;
+                // Increment count of items for this entry.
+                counts[c]++;
+            } else {
+                // bucket already created, check if growth is required
+                CharSequence[] sub = (CharSequence[]) lastBucket[c];
+                if (lastIndex[c] < SUBBUCKET_THRESHOLD &&
+                        lastIndex[c] == sub.length) {
+                    // Grow the sub-bucket to accomodate the new string.
+                    CharSequence[] tmp = new CharSequence[
+                            sub.length * SUBBUCKET_GROWTH_FACTOR];
+                    System.arraycopy(sub, 0, tmp, 0, sub.length);
+                    sub = tmp;
+                    // Update references to the last sub-bucket.
+                    lastBucket[c] = sub;
+                    Object[] bind = (Object[]) ptrs[c];
+                    bind[bind.length - 1] = sub;
+                } else if (lastIndex[c] == SUBBUCKET_THRESHOLD) {
+                    // Grow the bucket by adding a new sub-bucket.
+                    sub = new CharSequence[SUBBUCKET_START_SIZE];
+                    lastBucket[c] = sub;
+                    lastIndex[c] = 0;
+                    Object[] obind = (Object[]) ptrs[c];
+                    // Grow the bucket index by one each time.
+                    Object[] bind = new Object[obind.length + 1];
+                    System.arraycopy(obind, 0, bind, 0, obind.length);
+                    bind[bind.length - 1] = sub;
+                    ptrs[c] = bind;
+                }
+                // insert string in bucket
+                sub[lastIndex[c]] = s;
+                // point to next cell
+                lastIndex[c]++;
+                // increment count of items
+                counts[c]++;
+            }
+        }
+
+        /**
+         * Retrieve the trie node or bucket index for character <em>c</em>.
+         *
+         * @param  c  character for which to retrieve entry.
+         * @return  the entry for the given character, which may either be
+         *          another trie node, or a bucket index (of type Object[]).
+         */
+        public Object get(char c) {
+            return ptrs[c];
+        }
+
+        /**
+         * Set the trie node or bucket index for character <em>c</em>.
+         * Adjusts the "size" property associated with the character if
+         * needed.
+         *
+         * @param  c  character for which to store new entry.
+         * @param  o  the entry for the given character.
+         */
+        public void set(char c, Object o) {
+            ptrs[c] = o;
+            if (o instanceof Node) {
+                // flag to indicate pointer to trie node and not bucket
+                counts[c] = -1;
+            }
+        }
+
+        /**
+         * Returns the number of strings stored for the given character.
+         *
+         * @param  c  character for which to get count.
+         * @return  number of tail strings; -1 if child is a trie node.
+         */
+        public int size(char c) {
+            return counts[c];
+        }
+    }
+}
---
+++ test/org/burstsort4j/RedesignedBurstsortTest.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2009  Nathan Fiedler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * $Id$
+ */
+
+package org.burstsort4j;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for the RedesignedBurstsort class.
+ *
+ * @author Nathan Fiedler
+ */
+public class RedesignedBurstsortTest {
+
+    @Test
+    public void testArguments() {
+        RedesignedBurstsort.sort(null);
+        RedesignedBurstsort.sort(new String[0]);
+        String[] arr = new String[] { "a" };
+        RedesignedBurstsort.sort(arr);
+        arr = new String[] { "b", "a" };
+        RedesignedBurstsort.sort(arr);
+        assertTrue(Tests.isSorted(arr));
+        arr = new String[] { "c", "b", "a" };
+        RedesignedBurstsort.sort(arr);
+        assertTrue(Tests.isSorted(arr));
+    }
+
+    @Test
+    public void testDictWords() {
+        try {
+            // Use the large dictionary rather than the trivial one.
+            List<String> data = Tests.loadData("dictwords.gz", true);
+            Collections.shuffle(data);
+            String[] arr = data.toArray(new String[data.size()]);
+            System.out.format("\nDictionary words (large):\n");
+            RedesignedBurstsort.sort(arr, System.out);
+            assertTrue(Tests.isSorted(arr));
+        } catch (IOException ioe) {
+            fail(ioe.toString());
+        }
+    }
+
+// XXX
+//    @Test
+//    public void testDictWordsParallel() {
+//        try {
+//            // Use the large dictionary rather than the trivial one.
+//            List<String> data = Tests.loadData("dictwords.gz", true);
+//            Collections.shuffle(data);
+//            String[] arr = data.toArray(new String[data.size()]);
+//            try {
+//                RedesignedBurstsort.sortThreadPool(arr);
+//            } catch (InterruptedException ie) {
+//                fail(ie.toString());
+//            }
+//            assertTrue(Tests.isSorted(arr));
+//        } catch (IOException ioe) {
+//            fail(ioe.toString());
+//        }
+//    }
+
+    @Test
+    public void testSorted() {
+        try {
+            List<String> data = Tests.loadData();
+            Collections.sort(data);
+            String[] arr = data.toArray(new String[data.size()]);
+            System.out.format("\nDictionary words (sorted):\n");
+            RedesignedBurstsort.sort(arr, System.out);
+            assertTrue(Tests.isSorted(arr));
+        } catch (IOException ioe) {
+            fail(ioe.toString());
+        }
+    }
+
+    @Test
+    public void testReversed() {
+        try {
+            List<String> data = Tests.loadData();
+            Collections.sort(data);
+            Collections.reverse(data);
+            String[] arr = data.toArray(new String[data.size()]);
+            System.out.format("\nDictionary words (reversed):\n");
+            RedesignedBurstsort.sort(arr, System.out);
+            assertTrue(Tests.isSorted(arr));
+        } catch (IOException ioe) {
+            fail(ioe.toString());
+        }
+    }
+
+    @Test
+    public void testRepeated() {
+        // Make the size of the set large enough to burst buckets.
+        String[] arr = new String[131072];
+        Arrays.fill(arr, "abcdefghijklmnopqrstuvwxyz");
+        System.out.format("\nRepeated alphabet string:\n");
+        RedesignedBurstsort.sort(arr, System.out);
+        assertTrue(Tests.isRepeated(arr, "abcdefghijklmnopqrstuvwxyz"));
+    }
+
+    @Test
+    public void testRandom() {
+        List<String> data = Tests.generateData(131072, 64);
+        String[] arr = data.toArray(new String[data.size()]);
+        System.out.format("\nRandom strings:\n");
+        RedesignedBurstsort.sort(arr, System.out);
+        assertTrue(Tests.isSorted(arr));
+    }
+
+    @Test
+    public void testHamlet() {
+        try {
+            List<String> data = Tests.loadData("hamletwords");
+            Collections.shuffle(data);
+            String[] arr = data.toArray(new String[data.size()]);
+            System.out.format("\nHamlet words:\n");
+            RedesignedBurstsort.sort(arr, System.out);
+            assertTrue(Tests.isSorted(arr));
+        } catch (IOException ioe) {
+            fail(ioe.toString());
+        }
+    }
+
+    @Test
+    public void testDictCalls() {
+        try {
+            List<String> data = Tests.loadData("dictcalls.gz", true);
+            String[] arr = data.toArray(new String[data.size()]);
+            System.out.format("\nLibrary calls:\n");
+            RedesignedBurstsort.sort(arr, System.out);
+            assertTrue(Tests.isSorted(arr));
+        } catch (IOException ioe) {
+            fail(ioe.toString());
+        }
+    }
+}
